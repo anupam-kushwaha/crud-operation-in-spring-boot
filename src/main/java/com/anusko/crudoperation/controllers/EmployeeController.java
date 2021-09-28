de6a45614cd485ea5e0f27f3c014c5f5d0d05b3d
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-09-28 22:49:41 +0530

    Added method to handle request

--- src/main/java/com/anusko/crudoperation/controllers/EmployeeController.java
+++ src/main/java/com/anusko/crudoperation/controllers/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.anusko.crudoperation.controllers;
 
+import com.anusko.crudoperation.models.Employee;
 import com.anusko.crudoperation.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -16,5 +20,29 @@ public class EmployeeController {
     public String viewHomePage(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFromForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable (value = "id") long id, Model model) {
+        // get employee from service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // set employee as a model to pre-populate the form
+        model
+
     }
 }
