0e188330521e8054b8d71b97f4bed3c82f22ff4a
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-09-29 00:10:03 +0530

    Implemented employee service

--- src/main/java/com/anusko/crudoperation/services/EmployeeServiceImp.java
+++ src/main/java/com/anusko/crudoperation/services/EmployeeServiceImp.java
@@ -3,9 +3,12 @@ package com.anusko.crudoperation.services;
 import com.anusko.crudoperation.models.Employee;
 import com.anusko.crudoperation.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class EmployeeServiceImp implements EmployeeService{
 
     @Autowired
@@ -14,5 +17,26 @@ public class EmployeeServiceImp implements EmployeeService{
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if(optional.isPresent()) {
+            employee = optional.get();
+        }else {
+            throw new RuntimeException(" Employee not found for id::"+ id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
     }
 }
